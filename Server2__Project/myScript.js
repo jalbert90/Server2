07039d80f506298c1776f938be69028e32c2f506
jacob <jalbert90@gmail.com>
2023-10-22 22:56:22 -0400

    Added `fetch()`

--- Server2__Project/myScript.js
+++ Server2__Project/myScript.js
@@ -1,8 +1,20 @@
 let btn = document.getElementById("btn");
 let num = document.getElementById("num");
+let form = document.getElementById("search-form");
+let input = document.getElementById("search-form").elements["query"];
+let submit = document.getElementById("search-form").elements["search"];
+let results = document.getElementById("search-results");
 
 function btnPress() {
     num.innerHTML = Math.floor(Math.random() * 11)
 }
 
+function onSubmit(event) {
+    event.preventDefault();
+    let query = document.querySelector('[name="query"]').value;
+
+    fetch("/?query=" + query).then(response => { response.text() }).then(text => { console.log(text) });
+}
+
 btn.addEventListener("click", btnPress);
+form.addEventListener("submit", onSubmit);
