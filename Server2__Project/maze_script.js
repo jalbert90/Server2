b156222e1a3bb71bd1ede033cfbcd72a44d7c02c
jacob <jalbert90@gmail.com>
2024-05-01 19:13:26 -0400

    Made maze cell walls. Made `Maze` class.

--- Server2__Project/maze_script.js
+++ Server2__Project/maze_script.js
@@ -5,11 +5,31 @@ let maze_container = document.getElementsByClassName("maze_container")[0];
 const maze_dim = 10;
 const col_width = "50px ";
 maze_container.style["grid-template-columns"] = col_width.repeat(maze_dim);
+maze_cells = [];
+
+class Maze {
+    #maze_cells = [];
+    constructor(maze_dim, col_width) {
+        this.maze_dim = maze_dim;
+        this.col_width = col_width;
+        this.maze_container = document.getElementByClassName("maze_container")[0];
+    }
+
+    construct_skeleton() {
+        this.maze_container.style["grid-template-columns"] = this.col_width.repeat(this.maze_dim);
+        for (let row = 0; row < this.maze_dim; row++) {
+            for (let col = 0; col < this.maze_dim; col++) {
+                // Create Maze_Cell object.
+                // Add to matrix.
+            }
+        }
+    }
+}
 
 /**
  * Next steps:
  * 
- * Create matrix to hold maze cells.
+ * Create matrix to hold maze cells. Done.
  * Create maze cell walls.
  * Create game manager.
  * Possible classes: Maze, MazeCell
@@ -20,6 +40,7 @@ function create_maze_cell(row, col) {
     maze_cell.className = "maze_cell";
     maze_cell.innerHTML = 10 * row + (col + 1);
     maze_container.appendChild(maze_cell);
+    return maze_cell;
 }
 
 function delay(ms) {
@@ -31,10 +52,13 @@ function delay(ms) {
 
 async function generate_btn_click() {
     for (let row = 0; row < maze_dim; row++) {
+        let maze_cell_row = []
         for (let col = 0; col < maze_dim; col++) {
-            create_maze_cell(row, col);
-            await delay(50);
+            maze_cell = create_maze_cell(row, col);
+            maze_cell_row.push(maze_cell);
+            await delay(10);
         }
+        maze_cells.push(maze_cell_row);
     }
 }
 
